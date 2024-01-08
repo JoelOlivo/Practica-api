311748cfdb79c1dbf06c3c36ed011167116d41d4
JoelOlivo <139314609+JoelOlivo@users.noreply.github.com>
2024-01-08 16:53:21 -0500

    Creacion CRUD, get usuario

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import { obtenerChiste } from "./js/http-provider";
-
-import { obtenerUsuarios } from "./js/http-provider";
-
+// import { obtenerChiste } from "./js/http-provider";
+// import { obtenerUsuarios } from "./js/http-provider";
 // import { init } from "./js/chistes-page";
-import { init } from "./js/usuarios-page";
-
+// import { init } from "./js/usuarios-page";
+import * as CRUD from "./js/crud-provider";
 
 // obtenerChiste()
 // .then(console.log)
 // .catch(console.log)
 
-init();
+// init();
 
 // obtenerUsuarios()
+
+CRUD.getUsuario(1).then(console.log);
+
+
